bed6ccac7bf4cdaafd2e92fe2451034586699ece
Marco Piermatei <marco@MacBook-Pro-de-Marco.local>
2024-08-05 20:56:58 -0300

    3.01.2

--- src/components/ContactButton.js
+++ src/components/ContactButton.js
@@ -2,17 +2,21 @@ import React from 'react';
 
 const ContactButton = ({ show }) => { // 'show' para controlar la visibilidad
   return (
-    <button
+    <a
+      href="https://api.whatsapp.com/send/?phone=543415711104&text&type=phone_number&app_absent=0"
+      target="_blank" // Abre el enlace en una nueva pestaña
+      rel="noopener noreferrer" // Seguridad adicional para enlaces externos
       style={{
         opacity: show ? 1 : 0,
         transition: 'opacity 0.5s ease-in-out',
         pointerEvents: show ? 'auto' : 'none' 
       }}
-      className="fixed bottom-10 left-1/2 w-[90%] transform -translate-x-1/2 bg-[#3ECC33] text-white font-black py-4 px-6 rounded-3xl shadow-md hover:opacity-80 z-50"
+      className="fixed bottom-10 left-1/2 w-[90%] transform -translate-x-1/2 bg-[#3ECC33] text-white font-black py-4 px-6 rounded-3xl shadow-md hover:opacity-80 z-50 text-center" // Agregar text-center para alinear el texto
     >
       Contáctanos
-    </button>
+    </a>
   );
 };
 
 export default ContactButton;
+
--- src/services/sheetService.js
+++ src/services/sheetService.js
@@ -6,33 +6,24 @@ const API_KEY = 'AIzaSyCwVDEYoC8EL3gKF9VRuM4FDIeyX7aERgM';
 const BASE_URL = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/`;
 
 export const fetchCategories = async () => {
-  try {
-    const response = await axios.get(`${BASE_URL}Categories!A1:C100?key=${API_KEY}`);
-    const categories = response.data.values.slice(1).map(row => ({
-      id: row[0],
-      name: row[1],
-      imageUrl: row[2],
-    }));
-    return categories;
-  } catch (error) {
-    console.error("Error fetching categories:", error);
-    return [];
-  }
+  const response = await axios.get(`${BASE_URL}Categories!A1:C100?key=${API_KEY}`);
+  const categories = response.data.values.slice(1).map(row => ({
+    id: row[0],
+    name: row[1],
+    imageUrl: row[2],
+  }));
+  return categories;
 };
 
 export const fetchProducts = async () => {
-  try {
-    const response = await axios.get(`${BASE_URL}Products!A1:E100?key=${API_KEY}`);
-    const products = response.data.values.slice(1).map(row => ({
-      id: row[0],
-      name: row[1],
-      category: row[2],
-      description: row[3],
-      imageUrl: row[4],
-    }));
-    return products;
-  } catch (error) {
-    console.error("Error fetching products:", error);
-    return [];
-  }
+  const response = await axios.get(`${BASE_URL}Products!A1:F100?key=${API_KEY}`);
+  const products = response.data.values.slice(1).map(row => ({
+    id: row[0],
+    name: row[1],
+    category: row[2],
+    description: row[3],
+    imageUrl: row[4],
+    prices: JSON.parse(row[5] || '[]'),  // Convertir JSON string a array de objetos
+  }));
+  return products;
 };
