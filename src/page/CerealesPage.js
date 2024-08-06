1737c0645e6f28a99d333a0f17308961f98146c9
Marco Piermatei <marco@MacBook-Pro-de-Marco.local>
2024-08-05 22:41:36 -0300

    3.03.05

--- src/page/CerealesPage.js
+++ src/page/CerealesPage.js
@@ -1,41 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import SearchInput from '../components/SearchInput';
 import CategoryProductList from '../components/CategoryProductList';
-import products from '../data/products';
 import Breadcrumb from '../components/Breadcrumb';
-
+import { fetchProducts } from '../services/sheetService';
 
 const CerealesPage = () => {
   const [filteredProducts, setFilteredProducts] = useState([]);
+  const [allProducts, setAllProducts] = useState([]);
   const category = 'Cereales'; 
 
   useEffect(() => {
-    console.log('Products in useEffect:', products);
-    const initialFiltered = products.filter((product) =>
-      product.category.toLowerCase() === category.toLowerCase()
-    );
-    console.log('Initial filtered products:', initialFiltered);
-    setFilteredProducts(initialFiltered);
+    const fetchData = async () => {
+      try {
+        const productsData = await fetchProducts();
+        setAllProducts(productsData);
+        const initialFiltered = productsData.filter((product) =>
+          product.category.toLowerCase() === category.toLowerCase()
+        );
+        setFilteredProducts(initialFiltered);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+    fetchData();
   }, [category]);
 
   const handleSearch = (term) => {
-    console.log('Search term:', term);
-    const filtered = products.filter((product) =>
+    const filtered = allProducts.filter((product) =>
       product.name.toLowerCase().includes(term.toLowerCase()) &&
       product.category.toLowerCase() === category.toLowerCase()
     );
-    console.log('Filtered products after search:', filtered);
     setFilteredProducts(filtered);
   };
-
-  useEffect(() => {
-    console.log('Filtered products in useEffect:', filteredProducts);
-  }, [filteredProducts]);
 
   return (
     <div className="flex flex-col min-h-screen">
       <div className="flex-grow">
-
         <h1 className="text-3xl font-bold ml-4">
           {category}
         </h1>
