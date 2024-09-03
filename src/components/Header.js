614564d590337e974d10996365565123a3a804fe
Marco Piermatei <marco@MacBook-Pro-de-Marco.local>
2024-09-02 21:05:05 -0300

    Problema logo - V.7.01.01.003

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,17 @@
+// Header.jsx
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Logo from './icons/logo';
 
 const Header = () => {
   return (
-    <header className="flex justify-center items-center h-24 my-4">
-      <Link to="/" className="flex items-center">
-        <Logo className="lg:w-1/2 sm:w-auto" />
+    <header className="header">
+      <Link to="/" className="header-logo">
+        <Logo />
       </Link>
     </header>
   );
 };
 
 export default Header;
-
-
-
-
-
